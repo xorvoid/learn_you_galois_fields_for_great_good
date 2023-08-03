8d880bbd89b60cc44d3a173679852a73b9a84fc3
xorvoid <xorvoid@gmail.com>
2023-08-03 14:41:04 -0500

    Added comment

--- src/bin/gf_p_calc.rs
+++ src/bin/gf_p_calc.rs
@@ -7,6 +7,7 @@ use learn_you_galois_fields_for_great_good::gf_p::{GF, P};
 use learn_you_galois_fields_for_great_good::calc;
 
 fn main() {
+  // Simply call into the generic interactive calculator using our field
   let name = format!("GF({})", P);
   calc::interactive_calculator::<GF>(&name);
 }
