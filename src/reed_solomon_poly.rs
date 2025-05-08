64f0dc25e4f6f1028ea936550b1717c948db4152
xorvoid <xorvoid@gmail.com>
2025-05-08 12:30:35 -0700

    reed_solomon_poly.rs: editing the prose

--- src/reed_solomon_poly.rs
+++ src/reed_solomon_poly.rs
@@ -2,7 +2,7 @@ use crate::gf_256::GF;
 
 //// #### Polynomial Evaluation
 ////
-//// We will need a routine to evaluate polynomials. For this we'll accept an array of coefficients and
+//// First, we need a routine to evaluate polynomials. Our routine will take an array of coefficients and
 //// an evaluation point. We will use [Horner's Method](https://en.wikipedia.org/wiki/Horner%27s_method)
 //// for the algorithm. This allows us to do an evaluation in `O(n)` time.
 
@@ -28,74 +28,83 @@ fn poly_eval(coeffs: &[GF], xs: &[GF]) -> Vec<GF> {
 
 //// #### Polynomial Interpolation
 ////
-//// For polynomial interpolation, we'll recover the coefficients using an expansion from
-//// the newton polynomial. Note: this algorithm is a bit tricky. If you don't follow it right
-//// away, I'd suggest thinking about deriving it yourself directly and compare results. If you
-//// still are having difficulty, it's okay to skim this section. The important things are that
-//// (1) you know that polynomial interpolation is not magic, and (2) you understand where the
-//// runtime complexity comes from in the algorithm.
+//// For polynomial interpolation, we'll recover the coefficients by constructing a Newton Polynomial. But, unlike our
+//// practice examples, we'll store all polynomials in monomial form.
 ////
-//// A newton polynomial looks like:
+//// Note: this algorithm is a bit tricky. If you don't follow it right
+//// away, I'd suggest deriving it yourself directly and comparing results. If you
+//// still are having difficulty, it's okay to skim this section. The important parts are:
+//// (1) understanding that polynomial interpolation is not magic, and (2) understanding the
+//// runtime complexity of the algorithm
+////
+//// Recall that a Newton Polynomial has the form:
 ////
 //// ```
 //// p(x) = k0 + k1*(x-x0) + k2*(x-x0)*(x-x1) + k3*(x-x0)*(x-x1)*(x-x2) + ...
 //// ```
 ////
-//// Our algorithm will maintain two polynomials (stored in monomial coefficient form):
+//// For our implementation, we'll maintain two polynomials:
 ////
 //// 1. The `p(x)` polynomial constructed so far
 //// 2. The `z(x)` polynomial of `(x - c)` factors so far
 ////
-//// We can iteratively add a new point each step with the steps:
 ////
-//// 1. Solve for `k` by evaluating `p(x)` and `z(x)`
-//// 2. Update `p` as  `p = p + k * z`
-//// 3. Update `z` as `z = z * (x - c)`
+//// We can add new points by combining these polynomials (using a suitable `k`):
+//// ```
+//// p'(x) = p(x) + k*z(x)
+//// ```
 ////
-//// Since we'll maintain `p(x)` in monomial coefficient form, the final answer is
+//// For each new point `(xk, yk)`, we will use the steps:
+////
+//// 1. Solve for `k` with `k = (yk - p(xk))/z(xk)`
+//// 2. Update `p` with  `p = p + k * z`
+//// 3. Update `z` with `z = z * (x - xk)`
+////
+//// We will also maintain the polynomials `p(x)` and `z(x)` in monomial coefficient form. This makes them
+//// easy to combine and evaluate. It also means that the final answer is
 //// just `p(x)` whenever we finish adding new points.
 ////
-//// This algorithm has runtime complexity `O(n^2)` where `n` is the number of evaluations
-//// interpolated. This is because we're doing `O(n)` polynomial evaluations and updates
-//// in the inner loop, and we're looping over `n` points.
+//// Finally, notice that this algorithm has runtime complexity `O(n^2)` where `n` is the number of points
+//// interpolated. This is because the inner loop is `O(n)` and we're looping over `n` points.
+////
+//// Okay, let's code it:
 fn poly_interpolate(evals: &[GF], xs: &[GF]) -> Vec<GF> {
     // Sanity check
     assert_eq!(evals.len(), xs.len());
 
-    // Setup
+    // Initialize our polynomials to p(x) = 0 and z(x) = 1
     let n = xs.len();
     let mut p = vec![GF::new(0); n];
     let mut z = vec![GF::new(0); n];
     z[0] = GF::new(1);
 
-    // Loop over each evaluation point
+    // Loop over each point
     for i in 0..n {
         // Unpack the point for this iteration
-        let x0 = xs[i];
-        let y0 = evals[i];
+        let xk = xs[i];
+        let yk = evals[i];
 
-        // Step 1: Solve k = (y0 - p(x0)) / z(x0)
-        let k = (y0 - poly_eval_point(&p, x0)) / poly_eval_point(&z, x0);
+        // Step 1: Solve k = (yk - p(xk)) / z(xk)
+        let k = (yk - poly_eval_point(&p, xk)) / poly_eval_point(&z, xk);
 
         // Step 2: Update the polynomial: p' = p + k * z
         for j in 0..n {
             p[j] = p[j] + k * z[j];
         }
 
-        // Step 3: Update the "zero" polynomial: z' = z * (x - x0)
+        // Step 3: Update the z polynomial: z' = z * (x - xk)
         //
         // Let's simplify a bit first by distributing:
-        //   z' = z * x - z * x0
+        //   z' = z * x - z * xk
         //
         // Now notice that multiplying by `x` is a shift of the coefficients.
         // This leads to the form we'll use:
-        //   z' = shift(z) + (-x0)*z
+        //   z' = shift(z) - xk * z
         //
-        let x0_neg = x0.negate();
         for j in (1..n).rev() { // update in reverse
-            z[j] = z[j-1] + x0_neg * z[j];
+            z[j] = z[j-1] - xk * z[j];
         }
-        z[0] = x0_neg * z[0];
+        z[0] = GF::new(0) - xk * z[0];
     }
 
     // The final result is just `p`
@@ -103,20 +112,18 @@ fn poly_interpolate(evals: &[GF], xs: &[GF]) -> Vec<GF> {
 }
 
 //// #### Reed-Solomon Encoding and Decoding
-//// Okay, time for encoding and decoding functions. Similar to how we built fields, we'll be hardcoding the parameters for this
-//// particular Reed-Solomon code. Feel free to change them and experiment!
-
-const K: usize = 3;                     // Number of data elements (before encoding)
-const N: usize = 5;                     // Number of encoded elements
+//// Now, we'll implement the encoding and decoding functions. First, we'll to define a few parameters:
+const K: usize = 3;  // Number of data elements (before encoding)
+const N: usize = 5;  // Number of encoded elements
 
 //// We also need to specify the evaluation points. We need to have `N` of these.
 //// For this variant of Reed-Solomon, it really doesn't matter what we pick. They just need to be
-//// distinct elements of `GF(256)`. We choose: `[42, 222, 2, 8, 99]`.
+//// distinct elements of `GF(256)`:
 const X: [GF; 5] = [
     GF::new(42), GF::new(222), GF::new(2), GF::new(8), GF::new(99)
 ];
 
-//// Let's write the encode routine. This is just a trivial wrapper around polynomial evaluation!
+//// The encode routine is just a simple wrapper around polynomial evaluation
 pub fn reed_solomon_poly_encode(data: &[GF]) -> Vec<GF> {
     // Sanity check
     assert_eq!(data.len(), K);
@@ -126,9 +133,8 @@ pub fn reed_solomon_poly_encode(data: &[GF]) -> Vec<GF> {
     poly_eval(data, &X)
 }
 
-//// Let's write the decode routine. We'll specify erasures using `Some(x)` and `None` in Rust. This is not
-//// a common way to do it, but it illustrates the idea of erasures very well. Also, we have the possibility
-//// that decode could fail if too many erasures happen. This is indicated by returning None.
+//// For decoding, we'll specify presence/erasure using the `Option<GF>` type. If present, we have `Some(x)` and otherwise `None`.
+//// This is not a common way to do it, but it illustrates the idea of erasures very well.
 pub fn reed_solomon_poly_decode(encoded: &[Option<GF>]) -> Option<Vec<GF>> {
     // Sanity check
     assert_eq!(encoded.len(), N);
@@ -159,76 +165,103 @@ pub fn reed_solomon_poly_decode(encoded: &[Option<GF>]) -> Option<Vec<GF>> {
 
 //// #### Testing Time
 ////
-//// You always write tests for your code too... right? Riiiight!? I always do of course (**wink**)
-
+////
+//// First, we'll test that polynomial evaluation works correctly:
 #[cfg(test)]
-mod tests {
-    use super::*;
+#[test]
+fn test_poly_eval() {
+    assert_eq!(poly_eval_point(&[], GF::new(1)), GF::new(0));
+    assert_eq!(poly_eval_point(&[GF::new(2)], GF::new(2)), GF::new(2));
+    assert_eq!(poly_eval_point(&[GF::new(2), GF::new(2)], GF::new(3)), GF::new(4));
+    assert_eq!(poly_eval_point(&[GF::new(46), GF::new(198), GF::new(0), GF::new(89)], GF::new(201)), GF::new(126));
+}
 
-    #[test]
-    fn test_poly_eval() {
-        assert_eq!(poly_eval_point(&[], GF::new(1)), GF::new(0));
-        assert_eq!(poly_eval_point(&[GF::new(2)], GF::new(2)), GF::new(2));
-        assert_eq!(poly_eval_point(&[GF::new(2), GF::new(2)], GF::new(3)), GF::new(4));
-        assert_eq!(poly_eval_point(&[GF::new(46), GF::new(198), GF::new(0), GF::new(89)], GF::new(201)), GF::new(126));
+//// And we'll test polynomial interpolation
+#[cfg(test)]
+#[test]
+fn test_poly_interpolate() {
+    let poly = poly_interpolate(
+        &[GF::new(21), GF::new(21), GF::new(19)],
+        &[GF::new(4), GF::new(5), GF::new(6)]
+    );
+    assert_eq!(poly, vec![GF::new(1), GF::new(1), GF::new(1)]);
+
+    let poly = poly_interpolate(
+        &[GF::new(57), GF::new(56), GF::new(49)],
+        &[GF::new(4), GF::new(5), GF::new(6)]
+    );
+    assert_eq!(poly, vec![GF::new(1), GF::new(2), GF::new(3)]);
+}
+
+//// We should be able to decode up to 2-element erasures. But, 3-element erasures will fail.
+////
+//// Let's test each possibility.
+#[cfg(test)]
+fn encode_decode_all(data: &[GF], expected_enc: &[GF]) {
+    // encode
+    let enc = reed_solomon_poly_encode(data);
+    assert_eq!(enc, expected_enc);
+    let recv_all: Vec<_> = enc.iter().map(|x| Some(*x)).collect();
+
+    // decode with no erasures: success!
+    assert_eq!(reed_solomon_poly_decode(&recv_all), Some(data.to_vec()));
+
+    // decode with all one element erasures: success!
+    for i in 0..N {
+        let mut recv = recv_all.clone();
+        recv[i] = None;
+        assert_eq!(reed_solomon_poly_decode(&recv), Some(data.to_vec()));
     }
 
-    #[test]
-    fn test_poly_interpolate() {
-        let poly = poly_interpolate(
-            &[GF::new(21), GF::new(21), GF::new(19)],
-            &[GF::new(4), GF::new(5), GF::new(6)]
-        );
-        assert_eq!(poly, vec![GF::new(1), GF::new(1), GF::new(1)]);
-
-        let poly = poly_interpolate(
-            &[GF::new(57), GF::new(56), GF::new(49)],
-            &[GF::new(4), GF::new(5), GF::new(6)]
-        );
-        assert_eq!(poly, vec![GF::new(1), GF::new(2), GF::new(3)]);
+    // decode with all two element erasures: success!
+    for i in 0..N {
+        for j in (i+1)..N {
+            let mut recv = recv_all.clone();
+            recv[i] = None;
+            recv[j] = None;
+            assert_eq!(reed_solomon_poly_decode(&recv), Some(data.to_vec()));
+        }
     }
 
-    #[test]
-    fn test_encode_decode() {
-        let data = vec![GF::new(100), GF::new(150), GF::new(200)];
-
-        // encode
-        let enc = reed_solomon_poly_encode(&data);
-        assert_eq!(enc, vec![GF::new(160), GF::new(135), GF::new(94), GF::new(104), GF::new(194)]);
-        let recv_all: Vec<_> = enc.iter().map(|x| Some(*x)).collect();
-
-        // decode with no erasures
-        assert_eq!(reed_solomon_poly_decode(&recv_all), Some(data.clone()));
-
-        // erase element 0 and decode
-        let mut recv = recv_all.clone();
-        recv[0] = None;
-        assert_eq!(reed_solomon_poly_decode(&recv), Some(data.clone()));
-
-        // erase elements 1,2 and decode
-        let mut recv = recv_all.clone();
-        recv[1] = None;
-        recv[2] = None;
-        assert_eq!(reed_solomon_poly_decode(&recv), Some(data.clone()));
-
-        // erase elements 2,4 and decode
-        let mut recv = recv_all.clone();
-        recv[2] = None;
-        recv[4] = None;
-        assert_eq!(reed_solomon_poly_decode(&recv), Some(data.clone()));
-
-        // erase elements 1,2,3 and decode will fail
-        let mut recv = recv_all.clone();
-        recv[1] = None;
-        recv[2] = None;
-        recv[3] = None;
-        assert_eq!(reed_solomon_poly_decode(&recv), None);
-
-        // erase elements 0,2,4 and decode will fail
-        let mut recv = recv_all.clone();
-        recv[0] = None;
-        recv[2] = None;
-        recv[4] = None;
-        assert_eq!(reed_solomon_poly_decode(&recv), None);
+    // decode with all three element erasures: failure!
+    for i in 0..N {
+        for j in (i+1)..N {
+            for k in (j+1)..N {
+                let mut recv = recv_all.clone();
+                recv[i] = None;
+                recv[j] = None;
+                recv[k] = None;
+                assert_eq!(reed_solomon_poly_decode(&recv), None);
+            }
+        }
     }
+}
+
+//// Now we can test a bunch of different data vectors:
+#[cfg(test)]
+#[test]
+fn test_encode_decode() {
+    // trivial
+    encode_decode_all(
+        &[GF::new(0), GF::new(0), GF::new(0)],
+        &[GF::new(0), GF::new(0), GF::new(0), GF::new(0), GF::new(0)],
+    );
+
+    // ones
+    encode_decode_all(
+        &[GF::new(1), GF::new(1), GF::new(1)],
+        &[GF::new(3), GF::new(161), GF::new(7), GF::new(73), GF::new(160)],
+    );
+
+    // pattern
+    encode_decode_all(
+        &[GF::new(100), GF::new(150), GF::new(200)],
+        &[GF::new(160), GF::new(135), GF::new(94), GF::new(104), GF::new(194)],
+    );
+
+    // random
+    encode_decode_all(
+        &[GF::new(216), GF::new(196), GF::new(171)],
+        &[GF::new(81), GF::new(157), GF::new(209), GF::new(193), GF::new(105)],
+    );
 }
